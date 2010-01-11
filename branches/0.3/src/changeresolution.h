f56c28be8dc69a7feffeeee4fad3584905ba1050
M.Brekhof <M.Brekhof@1f312e42-e665-11dd-a971-7dbc132099af>
2010-01-11 19:34:21 +0000

    branch release for testing the 0.3
    git-svn-id: https://gappman.googlecode.com/svn@102 1f312e42-e665-11dd-a971-7dbc132099af

---
+++ branches/0.3/src/changeresolution.h
@@ -0,0 +1,34 @@
+/**
+ * \file changeresolution.h
+ * \brief Changes X screen resolution using the Xrandr extension
+ * 
+ *
+ * GPL v2
+ *
+ * Authors:
+ *   Martijn Brekhof <m.brekhof@gmail.com>
+ */
+
+/**
+* Enumeration for error types.
+* Currently supported types are: 
+* SUCCES: no error
+* NO_RANDR_EXTENSION: X server has no support for the RANDR extension
+* NO_SCREEN_CONFIGURATION: could not get a screen configuration for the current display. This means that the xrandr extension is not available for the current screen.
+* SIZE_NOT_AVAILABLE: provided width x height is not available
+* RATE_NOT_AVAILABLE: could not determine a proper rate for the new screen size
+*/
+enum error_types {
+  SUCCES,
+  NO_RANDR_EXTENSION,
+  NO_SCREEN_CONFIGURATION,
+  SIZE_NOT_AVAILABLE,
+  RATE_NOT_AVAILABLE
+} error_type;
+
+/**
+* \brief tries to change the display resolution to width x height.
+* \param width new display width
+* \param height new display height
+*/
+int changeresolution (int width, int height);
