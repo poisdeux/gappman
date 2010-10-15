d370c9c7c8c797a95b148dfc5a69f1b9fe17d48d
M.Brekhof <M.Brekhof@1f312e42-e665-11dd-a971-7dbc132099af>
2010-10-15 17:28:49 +0000

    Initial import of app to test the daemon
    git-svn-id: https://gappman.googlecode.com/svn@231 1f312e42-e665-11dd-a971-7dbc132099af

---
+++ trunk/applets/netman/daemon/test_daemon.c
@@ -0,0 +1,59 @@
+#include <dbus/dbus-glib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static void lose (const char *fmt, ...) G_GNUC_NORETURN G_GNUC_PRINTF (1, 2);
+static void lose_gerror (const char *prefix, GError *error) G_GNUC_NORETURN;
+
+static void
+lose (const char *str, ...)
+{
+  va_list args;
+
+  va_start (args, str);
+
+  vfprintf (stderr, str, args);
+  fputc ('\n', stderr);
+
+  va_end (args);
+
+  exit (1);
+}
+
+static void
+lose_gerror (const char *prefix, GError *error) 
+{
+  lose ("%s: %s", prefix, error->message);
+}
+
+
+int
+main (int argc, char **argv)
+{
+  DBusGConnection *bus;
+  DBusGProxy *remote_object;
+  GError *error = NULL;
+  guint i;
+  gint status;
+
+  g_type_init ();
+
+  bus = dbus_g_bus_get (DBUS_BUS_SESSION, &error);
+  if (!bus)
+    lose_gerror ("Couldn't connect to session bus", error);
+  
+  remote_object = dbus_g_proxy_new_for_name (bus,
+					     "gappman.NetmanService",
+					     "/GmNetmand",
+					     "gappman.NetmanInterface");
+
+  if (!dbus_g_proxy_call (remote_object, "RunCommand", &error,
+			  G_TYPE_STRING, "echo", G_TYPE_STRING, "arg1", G_TYPE_INVALID,
+			  G_TYPE_INT, &status, G_TYPE_INVALID))
+    lose_gerror ("Failed to complete RunCommand", error);
+
+
+  g_object_unref (G_OBJECT (remote_object));
+
+  exit(0);
+}
