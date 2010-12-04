5bc974cb9dd2ffd90909284a2f2a4ddf1cb1782d
M.Brekhof <M.Brekhof@1f312e42-e665-11dd-a971-7dbc132099af>
2010-12-04 20:53:37 +0000

    Added func to kill netmand when gappman quits
    git-svn-id: https://gappman.googlecode.com/svn@308 1f312e42-e665-11dd-a971-7dbc132099af

--- trunk/tests/rungappman.sh
+++ trunk/tests/rungappman.sh
@@ -3,8 +3,11 @@
 [ ! -x ./tests/rungappman.sh ] && echo "Error: script must be executed from package toplevel directory" && exit 1
 
 ./applets/netman/daemon/gm_netmand &
-[ $? -ne 0 ] && echo "Error: gm_netmand failed to start" && exit 1
+GM_NETMAND_PID=$?
+[ $GM_NETMAND_PID -ne 0 ] && echo "Error: gm_netmand failed to start" && exit 1
 
 ./appmanager/gappman --width 400 --height 300 --conffile xml-config-files/conf.xml \
     --gtkrc gtk-config/gtkrc --windowed
 [ $? -ne 0 ] && echo "Error: gappman failed to start" && exit 1
+
+kill $GM_NETMAND_PID
