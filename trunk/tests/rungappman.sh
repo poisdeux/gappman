4f6fcb7b458a942540a9f98a7df1d8ba63f61f2c
Martijn Brekhof <m.brekhof@gmail.com>
2011-12-27 23:05:11 +0100

    Removed explicit loading of gtkrc files.

--- trunk/tests/rungappman.sh
+++ trunk/tests/rungappman.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-[ ! -x ./tests/rungappman.sh ] && echo "Error: script must be executed from package toplevel directory" && exit 1
+[ ! -x ./tests/rungappman.sh ] && echo "Error: script must be executed from package toplevel directory as follows:
+./tests/rungappman.sh" && exit 1
 
-./appmanager/gappman --width 400 --height 300 --conffile xml-config-files/conf.xml \
+GTK2_RC_FILES=./gtk-config/gtkrc ./appmanager/gappman --width 400 --height 300 --conffile xml-config-files/conf.xml \
     --gtkrc gtk-config/gtkrc --windowed
 [ $? -ne 0 ] && echo "Error: gappman failed to start" && exit 1
