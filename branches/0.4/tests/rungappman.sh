7e4226834bc1462e4f3d01decaa27d36e4a44550
Martijn Brekhof <m.brekhof@gmail.com>
2011-12-30 15:58:36 +0100

    Deleted old dirs from subversion version control

--- branches/0.4/tests/rungappman.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-[ ! -x ./tests/rungappman.sh ] && echo "Error: script must be executed from package toplevel directory" && exit 1
-
-./appmanager/gappman --width 400 --height 300 --conffile xml-config-files/conf.xml \
-    --gtkrc gtk-config/gtkrc --windowed
-[ $? -ne 0 ] && echo "Error: gappman failed to start" && exit 1
